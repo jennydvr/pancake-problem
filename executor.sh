eab980531ba85bc9e0d677adedc83e7818d66e8b
Christian Chomiak <christianchomiak@gmail.com>
2012-10-23 21:04:49 -0430

    Scripts arreglados

--- executor.sh
+++ executor.sh
@@ -6,13 +6,13 @@
 #  Created by Jenny Valdez on 23/10/12.
 #  Copyright (c) 2012 Jenny Valdez. All rights reserved.
 
-make clean
-make
+#make clean
+#make
 
 temp=$1
 temp="${temp%p_*}"
 temp="${temp##*_}"
-temp+=".txt"
+temp="$temp.txt"
 echo $temp
 
 #echo "AStar"
@@ -30,15 +30,15 @@ echo $temp
 
 echo "IDAStar"
 echo "    w = 1..."
-./pancake $1 1 1 > $temp
+./pancakes $1 1 1 > $temp
 ./promedio.sh $temp
 
 echo "    w = 5..."
-./pancake $1 5 1 > $temp
+./pancakes $1 5 1 > $temp
 ./promedio.sh $temp
 
 echo "    w = 10..."
-./pancake $1 10 1 > $temp
+./pancakes $1 10 1 > $temp
 ./promedio.sh $temp
 
 remove $temp
