b02c85b4c1e102971896fda6c759b0056b7bfda4
Jenny Valdez <jedaniv@gmail.com>
2012-10-23 20:38:50 -0430

    Cambiecito

--- executor.sh
+++ executor.sh
@@ -15,18 +15,18 @@ temp="${temp##*_}"
 temp+=".txt"
 echo $temp
 
-echo "AStar"
-echo "    w = 1..."
-./pancake $1 1 0 > $temp
-./promedio.sh $temp
+#echo "AStar"
+#echo "    w = 1..."
+#./pancake $1 1 0 > $temp
+#./promedio.sh $temp
 
-echo "    w = 5..."
-./pancake $1 5 0 > $temp
-./promedio.sh $temp
+#echo "    w = 5..."
+#./pancake $1 5 0 > $temp
+#./promedio.sh $temp
 
-echo "    w = 10..."
-./pancake $1 10 0 > $temp
-./promedio.sh $temp
+#echo "    w = 10..."
+#./pancake $1 10 0 > $temp
+#./promedio.sh $temp
 
 echo "IDAStar"
 echo "    w = 1..."
